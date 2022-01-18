f82a591dd81005513e772d131e80948a324e83bc
Dmitrij Kolotusha <suvstreet@gmail.com>
2022-01-18 01:10:14 +0100

    add json server

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # test_task
 
 https://test-task-25ab8.web.app/
+
+# Запустить локально 
+
+В терменале вводим команду "$ npm run server"
+
+Потом запускаем само приложение "$ npm run serve"
--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+// axios.defaults.baseURL = process.env.VUE_APP_OTHER_URL
+axios.defaults.baseURL = 'http://localhost:3000'
 
 export default {
   async post(url, value) {
-    const { data } = await axios.post(`${url}.json`, value)
+    const { data } = await axios.post(`${url}`, value)
     return data.name
   },
 
   async get(url) {
-    const { data } = await axios.get(`${url}.json`)
+    const { data } = await axios.get(`${url}`)
     return data
   },
 
   async put(url, idx, value) {
-    await axios.put(`${url}/${idx}.json`, value)
+    await axios.put(`${url}/${idx}`, value)
   },
 
   async delete(url, idx) {
-    await axios.delete(`${url}/${idx}.json`)
+    await axios.delete(`${url}/${idx}`)
   },
 }
